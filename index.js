2dabae1057202a9e1f58680c93d481324601a2c9
tjoeunis_student <smartz456@gmail.com>
2024-10-28 17:43:32 +0900

    반응형 찐막

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 $(document).ready(function () {
-    let Swiper = new Swiper('.swiper2', {
-        slidesPerView:1,
-        centeredSlides: true,
-        loop: true,
-        grabCursor:true,
-        spaceBetween: 100,
-        nextEl: '.swiper-button-next2',
-        prevEl: '.swiper-button-prev2',
-        autoplay:{ delay: 3000 },
-    });
-});
-
-$(document).ready(function () {
     let i = 0;
     let m = 0;
     let count = $(".main_img>li").length;
     let swiper = new Swiper('.swiper', {
-        slidesPerView:3,
+        slidesPerView:1,
         centeredSlides: true,
         loop: true,
         grabCursor:true,
-        spaceBetween: 100,
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
         autoplay:{ delay: 3000 },
+        breakpoints: {
+        
+            768: {
+            slidesPerView: 3,  //브라우저가 768보다 클 때
+            spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,  //브라우저가 1024보다 클 때
+            spaceBetween: 50,
+            },
+        },
     });
 
 
@@ -54,8 +51,8 @@ $(".newcont_next").click(function(){
         $(".new_slide").css({"margin-left":"-18%"});
     });
 });
-});
-$(document).ready(function (){
+
+
     $("#header").mouseenter(function () {
         $("#header").stop().animate({"height" :"213px"});
         $(".sub").stop().fadeIn();
@@ -64,7 +61,7 @@ $(document).ready(function (){
         $("#header").stop().animate({"height" :"143px"});
         $(".sub").stop().fadeOut();
     });
-    });
+
 
 // $(".mens_inlist>li").click(function () {
 //     let list = $(this).index();
@@ -80,3 +77,5 @@ $(document).ready(function (){
 //             $(".new_slide").css({"margin-left":"-20%"});
 //         });
 // });
+});
+
